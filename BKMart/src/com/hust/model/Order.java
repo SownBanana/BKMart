8d79f5e2a73e4a52936e61502db1e56e50befb01
SownBanana <s.v.o.a.26@gmail.com>
2019-05-10 18:11:01 +0700

    v1

---
+++ BKMart/src/com/hust/model/Order.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hust.model;
+
+/**
+ *
+ * @author SownBanana
+ */
+public class Order {
+    private int id;             //mã hoá đơn
+    private Product name;       //sản phẩm
+    private int quantity;         //số lượng sản phẩm
+
+}
---
+++ BKMart/src/com/hust/model/Product.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hust.model;
+
+/**
+ *
+ * @author SownBanana
+ */
+public class Product {
+    public static double MSRP;          //giá niêm yết
+}
